ee6e1089196912b60dc7485d9d4e552793854b37
Victor Osinaga <osinagavictor4@gmail.com>
2024-08-15 07:33:39 -0300

    envs

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     back_panel_url_dev : import.meta.env.VITE_BACK_PANEL_URL_DEV,
     back_panel_url_prod : import.meta.env.VITE_BACK_PANEL_URL_PROD,
 
-    // BACK LEGACY STORE ADMIN
+    // BACK LEGACY STORE ADMINN
     back_legacy_admin_url_dev : import.meta.env.VITE_BACK_LEGACY_ADMIN_URL_DEV,
     back_legacy_admin_url_prod : import.meta.env.VITE_BACK_LEGACY_ADMIN_URL_PROD,
 
