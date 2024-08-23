07e0ee9ca609bc3c0292f691c7b524925eedf9bf
Victor Osinaga <osinagavictor4@gmail.com>
2024-08-22 23:58:34 -0300

    fix vista sucursales 2

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -181,7 +181,7 @@ export default function Navigation() {
                             </svg>
                         )}
                     </Link>
-                    {/* {showSubmenuColores && (
+                    {showSubmenuColores && (
                         <ul id='subMenuAjustes' className='submenuAjustes'>
                             <li className={`w-100 navLink ${getLinkClassName('tienda')}`}>
                                 <Link to={'/admin/ajustes/tienda'} onClick={() => handleSectionClick('tienda')}>
@@ -191,16 +191,16 @@ export default function Navigation() {
                                     <span className={`ms-3`}>Mi tienda</span>
                                 </Link>
                             </li>
-                            <li className={`w-100 navLink ${getLinkClassName('panel')}`}>
+                            {/* <li className={`w-100 navLink ${getLinkClassName('panel')}`}>
                                 <Link to={'/admin/ajustes/panel'} onClick={() => handleSectionClick('panel')}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="svgSize text-gray">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"></path>
                                     </svg>
                                     <span className={`ms-3`}>Estilos panel</span>
                                 </Link>
-                            </li>
+                            </li> */}
                         </ul>
-                    )} */}
+                    )}
 
                 </li>
             </ul>
